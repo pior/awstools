4426d4c5557248ea790d1a5a72538c3843404742
Pior Bastida <pbastida@socialludia.com>
2012-11-29 17:07:25 -0500

    added license header to source files

---
+++ awstools/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 Ludia Inc.
+# This software is licensed as described in the file LICENSE, which
+# you should have received as part of this distribution.
+# Author: Pior Bastida <pbastida@socialludia.com>
+
