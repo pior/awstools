ad7676e131ab4c88d8cd19e65386d4f7997bffb1
Pior Bastida <pbastida@socialludia.com>
2012-11-29 17:04:11 -0500

    added license header in source files

--- awstools/cfntemplate.py
+++ awstools/cfntemplate.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 Ludia Inc.
+# This software is licensed as described in the file LICENSE, which
+# you should have received as part of this distribution.
+# Author: Pior Bastida <pbastida@socialludia.com>
+
 import json
 import os
 
