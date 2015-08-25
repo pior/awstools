a66bddf35391bde755261772feb0b93f1fc236f6
Pior Bastida <pior@pbastida.net>
2015-08-24 23:05:24 -0400

    Remove the last stupid copyright headers

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2012 Ludia Inc.
-# This software is licensed as described in the file LICENSE, which
-# you should have received as part of this distribution.
-# Author: Pior Bastida <pbastida@ludia.com>
-
 from setuptools import setup, find_packages
 import codecs
 
