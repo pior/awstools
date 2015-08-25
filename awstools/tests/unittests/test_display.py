6b9629721177094514f9d3f9d33f32bce202ec86
Pior Bastida <pior@pbastida.net>
2015-08-24 23:26:28 -0400

    Fix another test

--- awstools/tests/unittests/test_display.py
+++ awstools/tests/unittests/test_display.py
@@ -19,16 +19,10 @@ class TestDisplay(unittest.TestCase):
         stacksummary = boto.cloudformation.stack.Stack()
         stacksummary.stack_name = "stack_name"
         stacksummary.stack_status = "stack_status"
-        stacksummary.creation_time = "creation_time"
+        stacksummary.creation_time = datetime.datetime.fromtimestamp(0)
         stacksummary.description = "template_description"
 
         fmt = display.format_stack_summary(stack)
-        self.assertIn('stack_name', fmt)
-        self.assertIn('stack_status', fmt)
-        self.assertIn('1969-12-31 14:00:00', fmt)
-        self.assertIn('template_description', fmt)
-
-        fmt = display.format_stack_summary_short(stack)
         self.assertIn('stack_name', fmt)
         self.assertIn('stack_status', fmt)
         self.assertIn('1969-12-31 14:00:00', fmt)
@@ -40,7 +34,7 @@ class TestDisplay(unittest.TestCase):
         self.assertIn('1969-12-31 14:00:00', fmt)
         self.assertIn('template_description', fmt)
 
-        fmt = display.format_stack_summary_short(stacksummary)
+        fmt = display.format_stacks([stack])
         self.assertIn('stack_name', fmt)
         self.assertIn('stack_status', fmt)
         self.assertIn('1969-12-31 14:00:00', fmt)
